587ee7a725adb6811b4200e23eecee2e9f885c57
zendus <uzendujmike@gmail.com>
2021-09-08 10:59:52 +0100

    added permission classes

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Post, Comment
 from .serializers import PostSerializer, UserCommentSerializer
 
@@ -19,24 +20,28 @@ def api_overview(request):
     return Response(api_urls)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def all_posts(request):
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_post_by_id(request, pk):
     post_by_id = Post.objects.get(id=pk)
     serializer = PostSerializer(post_by_id, many=False)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_post_by_section(request, section):
     post_by_section = Post.objects.filter(section=section).order_by('pk')
     serializer = PostSerializer(post_by_section, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
+@permission_classes([IsAdminUser])
 def create_new_post(request):
     serializer = PostSerializer(data=request.data)
     if serializer.is_valid():
@@ -44,6 +49,7 @@ def create_new_post(request):
     return Response('Post created successfully')
 
 @api_view(['POST', 'GET'])
+@permission_classes([IsAdminUser])
 def update_post(request, pk):
     post = Post.objects.get(id=pk)
     if request.method == 'GET':
@@ -54,12 +60,14 @@ def update_post(request, pk):
     return Response(serializer.data)
 
 @api_view(['DELETE'])
+@permission_classes([IsAdminUser])
 def delete_post(request, pk):
     post = Post.objects.get(id=pk)
     post.delete()
     return Response(f'Post with Id {pk} deleted')
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_post_comments(request, pk):
     post = Post.objects.get(id=pk)
     comments = post.comment.all()
